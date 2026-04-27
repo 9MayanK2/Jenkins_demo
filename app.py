59d20c388b6c51a3df5dc10134fc0ec5b81cbec7
9MayanK2 <trivedimayank92@gmail.com>
2026-04-27 23:37:35 +0530

    Adding initial content

---
+++ app.py
@@ -0,0 +1,19 @@
+import flask from flask
+
+app=flask(__name__)
+
+@app.route("/",methods=['GET'])
+def home():
+    return "HOME directory"
+
+
+@app.route("/version",methods=['GET'])
+def version():
+    return "Version Directory"
+
+
+@app.route("/health",methods=['GET'])
+def health():
+    return "Health Directory"
+
+app.run(host='0.0.0.0', port=5500)
